130da3ae5c5736a67213b382f298028ad754a6e9
Ангелина Григорчук <ang.grigorchuk@gmail.com>
2019-04-04 14:18:22 +0400

    Added javadocs for ListGenerator

--- src/ListGenerator.java
+++ src/ListGenerator.java
@@ -1,6 +1,11 @@
 public class ListGenerator {
     private int[] list;
 
+    /**
+     * Генерация массива, заполняемого случайными числами от -list.size до list.size-1
+     * @param listSize - размер массива
+     * @return - возвращает сгенерированный массив
+     */
     public int[] generateList(int listSize) {
         this.list = new int[listSize];
         for (int i = 0; i < listSize; i++) {
@@ -9,10 +14,15 @@ public class ListGenerator {
         return list;
     }
 
+    /**
+     * Генерация массива, заполняемого случайными числами от -10 до 9
+     * @param listSize - размер массива
+     * @return - возвращает сгенерированный массив
+     */
     public int[] generatePredicate(int listSize) {
         this.list = new int[listSize];
         for (int i = 0; i < listSize; i++) {
-            list[i] = ((int)(Math.random()*25 - 13));
+            list[i] = ((int)(Math.random()*20 - 10));
         }
         return list;
     }
