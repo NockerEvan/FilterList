249cd63d98e33876df5abba3c1a12528851e73e0
Ангелина Григорчук <ang.grigorchuk@gmail.com>
2019-04-07 01:23:24 +0400

    Added unit tests (empty). Just a setup

---
+++ test/FilterListTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FilterListTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void iterate() {
+    }
+
+    @Test
+    public void removeAllNotInPredicate() {
+    }
+
+    @Test
+    public void addIfNotInPredicate() {
+    }
+
+    @Test
+    public void removeIfNotInPredicate() {
+    }
+}
